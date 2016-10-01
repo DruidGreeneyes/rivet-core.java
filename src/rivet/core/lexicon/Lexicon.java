1751dcb3ac6f748391cffbec4e08e9ec2e35635b
JBox <josh.carton@gmail.com>
2016-10-01 14:24:00 +0100

    remove get() from Lexicon

--- src/rivet/core/lexicon/Lexicon.java
+++ src/rivet/core/lexicon/Lexicon.java
@@ -1,14 +1,11 @@
 package rivet.core.lexicon;
 
-import pair.Pair;
 import rivet.core.labels.MapRIV;
 
 public interface Lexicon {
     public int count();
 
     public Lexicon add(final String word, final MapRIV riv);
-
-    public Pair<MapRIV, MapRIV> get(final String word);
 
     public MapRIV getLex(final String word);
 
