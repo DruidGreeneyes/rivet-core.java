eb08762f110993bb48305079b4fdd88e58c9bb60
JBox <josh.carton@gmail.com>
2017-05-16 15:26:11 -0500

    formatting

--- src/rivet/adapt/SOLR.java
+++ src/rivet/adapt/SOLR.java
@@ -12,55 +12,60 @@ import rivet.core.labels.MapRIV;
 import rivet.core.labels.ArrayRIV;
 
 public class SOLR {
-    public static MapRIV mapRIVFromCollections(final Collection<Object> indices,
-            final Collection<Object> values, final int size) {
-        final MapRIV res = MapRIV.empty(size);
-        final Iterator<Object> is = indices.iterator();
-        final Iterator<Object> vs = values.iterator();
-        while (is.hasNext() && vs.hasNext())
-            res.put((int) is.next(), (double) vs.next());
-        return res;
-    }
+  public static MapRIV mapRIVFromCollections(final Collection<Object> indices,
+                                             final Collection<Object> values,
+                                             final int size) {
+    final MapRIV res = MapRIV.empty(size);
+    final Iterator<Object> is = indices.iterator();
+    final Iterator<Object> vs = values.iterator();
+    while (is.hasNext() && vs.hasNext())
+      res.put((int) is.next(), (double) vs.next());
+    return res;
+  }
 
-    public static ColtRIV coltRIVFromCollections(
-            final Collection<Object> indices, final Collection<Object> values,
-            final int size) {
-        final ColtRIV res = ColtRIV.empty(size);
-        final Iterator<Object> is = indices.iterator();
-        final Iterator<Object> vs = values.iterator();
-        while (is.hasNext() && vs.hasNext())
-            res.put((int) is.next(), (double) vs.next());
-        return res;
-    }
+  public static ColtRIV coltRIVFromCollections(
+                                               final Collection<Object> indices,
+                                               final Collection<Object> values,
+                                               final int size) {
+    final ColtRIV res = ColtRIV.empty(size);
+    final Iterator<Object> is = indices.iterator();
+    final Iterator<Object> vs = values.iterator();
+    while (is.hasNext() && vs.hasNext())
+      res.put((int) is.next(), (double) vs.next());
+    return res;
+  }
 
-    public static HPPCRIV hppcRIVFromCollections(
-            final Collection<Object> indices, final Collection<Object> values,
-            final int size) {
-        final HPPCRIV res = HPPCRIV.empty(size);
-        final Iterator<Object> is = indices.iterator();
-        final Iterator<Object> vs = values.iterator();
-        while (is.hasNext() && vs.hasNext())
-            res.put((int) is.next(), (double) vs.next());
-        return res;
-    }
+  public static HPPCRIV hppcRIVFromCollections(
+                                               final Collection<Object> indices,
+                                               final Collection<Object> values,
+                                               final int size) {
+    final HPPCRIV res = HPPCRIV.empty(size);
+    final Iterator<Object> is = indices.iterator();
+    final Iterator<Object> vs = values.iterator();
+    while (is.hasNext() && vs.hasNext())
+      res.put((int) is.next(), (double) vs.next());
+    return res;
+  }
 
-    public static DenseRIV denseRIVFromCollections(
-            final Collection<Object> indices, final Collection<Object> values,
-            final int size) {
-        final DenseRIV res = DenseRIV.empty(size);
-        final Iterator<Object> is = indices.iterator();
-        final Iterator<Object> vs = values.iterator();
-        while (is.hasNext() && vs.hasNext())
-            res.put((int) is.next(), (double) vs.next());
-        return res;
-    }
+  public static DenseRIV denseRIVFromCollections(
+                                                 final Collection<Object> indices,
+                                                 final Collection<Object> values,
+                                                 final int size) {
+    final DenseRIV res = DenseRIV.empty(size);
+    final Iterator<Object> is = indices.iterator();
+    final Iterator<Object> vs = values.iterator();
+    while (is.hasNext() && vs.hasNext())
+      res.put((int) is.next(), (double) vs.next());
+    return res;
+  }
 
-    public static ArrayRIV arrayRIVFromCollections(
-            final Collection<Object> indices, final Collection<Object> values,
-            final int size) {
-        final Integer[] is = indices.toArray(new Integer[indices.size()]);
-        final Double[] vs = values.toArray(new Double[values.size()]);
-        return new ArrayRIV(ArrayUtils.toPrimitive(is),
-                ArrayUtils.toPrimitive(vs), size);
-    }
+  public static ArrayRIV arrayRIVFromCollections(
+                                                 final Collection<Object> indices,
+                                                 final Collection<Object> values,
+                                                 final int size) {
+    final Integer[] is = indices.toArray(new Integer[indices.size()]);
+    final Double[] vs = values.toArray(new Double[values.size()]);
+    return new ArrayRIV(ArrayUtils.toPrimitive(is),
+                        ArrayUtils.toPrimitive(vs), size);
+  }
 }
